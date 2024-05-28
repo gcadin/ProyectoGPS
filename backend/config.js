3cdeb511d2aaabff6d7cca3236716edb29b3ff37
Francisco <francisco.erices1901@alumnos.ubiobio.cl>
2024-05-28 07:13:14 -0400

    configuracion inicial y conexion con mongodb

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+const moongose = require('mongoose');
+require("dotenv").config();
+
+
+moongose
+.connect(process.env.MONGODB)
+.then(() => console.log('Connected to MONGODB'))
+.catch((error) => console.error(error));
