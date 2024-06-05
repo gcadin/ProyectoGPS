1ee66b8d65a40a72465972bb309ce08169658285
Diego Valderrama <67162892+Diegojgl@users.noreply.github.com>
2024-06-05 02:53:47 -0400

    estructura pagina inicio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
+import{ BrowserRouter, Routes, Route } from 'react-router-dom'
 
-
-import './App.css'
+import AuthLayout from './layout/AuthLayout'
+import Login from './pages/user/Login'
+import Contacto from './pages/Contacto'
+import Nosotros from './pages/Nosotros'
+import CustomCarousel from './components/Carousel'
 
 function App() {
 
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AuthLayout/> }>
+            <Route path='/' element={<> <CustomCarousel/> </>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/nosotros' element={<Nosotros/>}/>
+            <Route path='/contacto' element={<Contacto/>}/>
+          </Route>
+          
+        </Routes>
+      </BrowserRouter>
     
 
     </>
---
+++ frontend/src/pages/Nosotros.jsx
@@ -0,0 +1,10 @@
+const Nosotros = () => {
+    return (
+        <>
+
+            <h2>Nosotros</h2>
+        </>
+    )
+}
+
+export default Nosotros;
