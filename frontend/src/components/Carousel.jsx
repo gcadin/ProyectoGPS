549965e82f8b91add731baee7e325873599e6f21
Francisco <francisco.erices1901@alumnos.ubiobio.cl>
2024-07-22 00:18:47 -0400

    Eliminacion carpeta en

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -11,7 +11,7 @@ function CustomCarousel() {
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item >
-                <Image src='/banner2.jpg' width={1400} height={900}></Image>
+                <Image src='/banner2.jpg' width={1900} height={900}></Image>
               <Carousel.Caption>
                 <h3>Second slide label</h3>
               </Carousel.Caption>
